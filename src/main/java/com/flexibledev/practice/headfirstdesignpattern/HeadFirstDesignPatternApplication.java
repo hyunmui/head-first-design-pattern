2d97b9fd7a4e525d63a9e71a99a1e8a7cc96d8f0
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-07-06 09:55:35 +0900

    first commit

---
+++ src/main/java/com/flexibledev/practice/headfirstdesignpattern/HeadFirstDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.flexibledev.practice.headfirstdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadFirstDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadFirstDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexibledev/practice/headfirstdesignpattern/HeadFirstDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexibledev.practice.headfirstdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadFirstDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
